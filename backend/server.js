d07b87242e182b9f3f0e1bcd53e320fc0586bffc
Achref-930 <achrefbaghbagha@gmail.com>
2026-03-03 12:06:59 +0100

    feat: update dotenv configuration and use environment variable for backend URL

--- backend/server.js
+++ backend/server.js
@@ -43,7 +43,7 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: err.message || 'Internal server error' });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,8 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import dotenv from 'dotenv';
+dotenv.config();
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
@@ -12,7 +14,7 @@ export default defineConfig(({ mode }) => {
         historyApiFallback: true,
         proxy: {
           '/api': {
-            target: 'http://localhost:5000',
+            target: env.BACKEND_URL,
             changeOrigin: true,
           },
         },
